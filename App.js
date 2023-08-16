c536b1a8ecbdf32f389bdb2204ebf0da2d7244d0
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-16 11:45:39 +0300

    added tailwind

--- App.js
+++ App.js
@@ -5,12 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 
+//Screens
+import HomeScreen from './src/screens/HomeScreen';
+
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-lg">HomeScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
