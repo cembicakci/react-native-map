2df760c67817fe028d49c9d1cc40867a3e0e9d23
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-16 11:21:45 +0300

    redux-toolkit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Redux
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </Provider>
   );
 }
 
