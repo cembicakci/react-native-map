18a0db921945a5dad96e08f711e288a2e951d101
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-19 18:02:05 +0300

    navigation stack

--- src/components/NavigateCard.js
+++ src/components/NavigateCard.js
@@ -1,11 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+
+//Redux
+import { setDestination } from '../redux/slices/navSlice';
+import { useDispatch } from 'react-redux';
+
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const NavigateCard = () => {
+
+    const dispatch = useDispatch()
+    const navigation = useNavigation()
+
     return (
-        <View>
-            <Text>NavigateCard</Text>
-        </View>
+        <SafeAreaView className="bg-white flex-1">
+            <Text className="text-center py-5 text-xl">Good Morning</Text>
+            <View className="border-t border-gray-200 flex-shrink">
+                <View>
+                    <GooglePlacesAutocomplete
+                        placeholder='Where To?'
+                        styles={{
+                            container: {
+                                flex: 0,
+                                backgroundColor: 'white',
+                                paddingTop: 20
+                            },
+                            textInput: {
+                                fontSize: 18,
+                                borderRadius: 0,
+                                backgroundColor: '#DDDDDF'
+                            },
+                            textInputContainer: {
+                                paddingHorizontal: 20,
+                                paddingBottom: 0
+                            }
+                        }}
+                        minLength={2}
+                        enablePoweredByContainer={false}
+                        onPress={(data, details = null) => {
+                            // console.log(data, details);
+
+                            dispatch(setDestination({
+                                location: details.geometry.location,
+                                description: data.description
+                            }))
+
+                            navigation.navigate('RideOptionsCard')
+
+                        }}
+                        fetchDetails={true}
+                        query={{
+                            key: GOOGLE_MAPS_APIKEY,
+                            language: 'en',
+                        }}
+                        debounce={400}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
